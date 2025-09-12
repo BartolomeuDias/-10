5716732ec56c3971e4234d3011bea66da632a336
Daniel <makeevCoocieS_daniil@mail.ru>
2025-09-12 03:14:01 +0300

    Refactor Faculty class

--- Faculty.cs
+++ Faculty.cs
@@ -1,26 +1,31 @@
 using System;
+using System.Xml.Linq;
 
 public class Faculty {
 	private Employee dean;
-	private String name;
-	private Institute institutes;
+	private string name;
+	private List<Institute> institutes;
 
-	public Employee GetDean() {
-		return this.dean;
+    public Faculty(Employee dean, string name)
+    {
+        Dean = dean;
+        Name = name;
+        institutes = new List<Institute>();
+    }
+	public Employee Dean
+	{
+		get { return dean; }
+		set { dean = value; }
 	}
-	public void SetDean(ref Employee employee) {
-		this.dean = employee;
+	public string Name
+	{
+		get { return name; }
+		set { name = value; }
 	}
-	public String GetName() {
-		return this.name;
-	}
-	public void SetName(ref String name) {
-		this.name = name;
-	}
-	public Institute GetInstitutes() {
+	public List<Institute> GetInstitutes() {
 		return this.institutes;
 	}
-	public void SetInstitutes(ref Institute institutes) {
+	public void SetInstitutes(List<Institute> institutes) {
 		this.institutes = institutes;
 	}
 	public void AddInstitute(ref Institute institute) {
@@ -29,12 +34,6 @@ public class Faculty {
 	public void RemoveInstitute(ref Institute institute) {
 		throw new System.NotImplementedException("Not implemented");
 	}
-	public Faculty(ref Employee dean, ref String name) {
-		throw new System.NotImplementedException("Not implemented");
-	}
 
-	private University university;
-
-	private Employee leads;
 
 }
