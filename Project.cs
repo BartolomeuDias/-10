cd5a73a11f45cf244a4ea3e8c84c2d2f7cf01548
Daniel <makeevCoocieS_daniil@mail.ru>
2025-09-14 02:29:36 +0300

    Refactor Project

--- Project.cs
+++ Project.cs
@@ -1,29 +1,22 @@
 using System;
 
 public class Project {
-	private String name;
-	private Date startDate;
-	private Date endDate;
-
-	public String GetName() {
-		return this.name;
+	private string name;
+	private DateTime startDate;
+	private DateTime endDate;
+	public string Name
+    {
+		get { return name; }
+		set { name = value; }
 	}
-	public void SetName(ref String name) {
-		this.name = name;
+	public DateTime StartDate
+    {
+		get { return startDate; }
+		set { startDate = value; }
 	}
-	public Date GetStartDate() {
-		return this.startDate;
+	public DateTime EndDate
+    {
+		get { return endDate; }
+		set { endDate = value; }
 	}
-	public void SetStartDate(ref Date startDate) {
-		this.startDate = startDate;
-	}
-	public Date GetEndDate() {
-		return this.endDate;
-	}
-	public void SetEndDate(ref Date endDate) {
-		this.endDate = endDate;
-	}
-
-	private Research_associate[] research_associates;
-
 }
