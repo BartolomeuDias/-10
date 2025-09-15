e0686d012d2c0ce055ea93dfe46c3fed7b60bbb8
Daniel <makeevCoocieS_daniil@mail.ru>
2025-09-15 15:40:41 +0300

    Add folders with images, diagrams and PDF

---
+++ Dean.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+public class Dean : Employee
+{
+    public Dean(string name, string email): base(name, email)
+    {
+
+    }
+    public void HireEmployee(ResearchAssociate employee)
+    {
+        throw new System.NotImplementedException("Not implemented");
+    }
+    public void FireEmployee(ResearchAssociate employee)
+    {
+        throw new System.NotImplementedException("Not implemented");
+    }
+}
